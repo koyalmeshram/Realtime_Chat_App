e84466fe6b64abf8e24ce777ac29f10f183961db
Vaibhav <koyalmesh99@gmail.com>
2025-02-01 20:06:44 +0530

    Update socket URL to use Render backend URL

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import io from 'socket.io-client'
 import Chat from './Chat'
 import music from './mixkit-tile-game-reveal-960.wav'
 
-const socket = io.connect("http://localhost:1000")
+const socket = io.connect("https://realtime-chat-app-5mlq.onrender.com")
 
 
 const App = () => {
